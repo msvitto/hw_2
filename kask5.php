3c708f0129abca8db853b098ba6528c37462d35f
marina svitto <marinamironchik04@gmail.com>
2024-03-04 09:51:56 +0200

    make a functions longest($a, $b)

--- kask5.php
+++ kask5.php
@@ -17,5 +17,16 @@ function findShort(string $str):int{
     return $shortestLength;
 }
 echo findShort($str);
+//Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+//https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/php
 
+$a="ssa";
+$b="ss";
+function longest($a, $b){
+    $c= $a.$b;
+    $uniqueChars=array_unique(str_split($c));
+    sort($uniqueChars);
+    return implode('', $uniqueChars);
 
+}
+print_r(longest($a,$b));
